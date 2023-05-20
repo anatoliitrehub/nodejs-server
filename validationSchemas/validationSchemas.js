9ed2cf1a21e5d3a2ce5762a46bc37b56decddfcd
Trehub Anatolii <tridfoto@gmail.com>
2023-05-20 23:35:16 +0300

    update validation, app middleware

--- validationSchemas/validationSchemas.js
+++ validationSchemas/validationSchemas.js
@@ -5,6 +5,11 @@ const contactAddSchema = Joi.object({
   email: Joi.string().required(),
   phone: Joi.string().required(),
 });
-const contactUpdSchema = Joi.object().min(1);
+// const contactUpdSchema = Joi.object().min(1);
+const contactUpdSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+}).or("name", "email", "phone");
 
 module.exports = { contactAddSchema, contactUpdSchema };
